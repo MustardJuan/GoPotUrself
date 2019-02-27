ec7016d34358aede6a1ba06f4c29422ba2424f7d
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-02-27 15:57:54 -0600

    Updated Main
    Need to fix pointer/reference issue because Im a fucking scrub

--- main.go
+++ main.go
@@ -4,22 +4,30 @@ import "fmt"
 import "os"
 import "bufio"
 
+//Declared types for map of function calls made by user, must return string and take no args
+type vanilla func() string
+
 func main() {
-	cmdList := make([]string,4)
-	cmdList[0] = "ls"
-	cmdList[1] = "pwd"
-	cmdList[2] = "ifconfig"
-	cmdList[3] = "ps"
+	//Defined Map data structure of strings using type vanilla
+	//Key, Value similar to dictionary can have more added at a later time
+	cmdList := map[string] vanilla {
+	"ifconfig": ifconfig,
+	}
+	//Scanner class to indefinetly read in input from user
 	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("guestUser@HomeOffice:~$")
 	for scanner.Scan() {
+		fmt.Print("guestUser@HomeOffice:~$")
 		cmdLineInput := scanner.Text()
+		//Checks the input to the map to see if a function needs to be called
 		for cmds := range cmdList {
-			if  cmdLineInput == cmdList[cmds]{
-				fmt.Println("call correct cmd function")
+			if cmdLineInput == cmds{
+				fmt.Println("That's a command my dude")
+				fmt.Println(cmdList[cmds])
 			}
 		}
 	}
-
+	//error checking for the scanner
 	if scanner.Err() != nil {
 		fmt.Println("Oops dawg")
 	}
