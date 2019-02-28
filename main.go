387796d2ecbbb83c3472e67c3361b5203179701a
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-02-27 18:24:27 -0600

    Suitable main finally
    Made changes due to lack of understanding
    Reformatted some things
    Terminal is flush now and looks real and acts real

--- main.go
+++ main.go
@@ -5,30 +5,30 @@ import "os"
 import "bufio"
 
 //Declared types for map of function calls made by user, must return string and take no args
-type vanilla func() string
+type vanilla func(*string)
 
 func main() {
+	output := ""
 	//Defined Map data structure of strings using type vanilla
 	//Key, Value similar to dictionary can have more added at a later time
 	cmdList := map[string] vanilla {
-	"ifconfig": ifconfig,
-	"ps": ps,
+		"ifconfig": Ifconfig,
+		"ps": Ps,
 	}
 	//Scanner class to indefinetly read in input from user
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("guestUser@HomeOffice:~$")
 	for scanner.Scan() {
-		fmt.Print("guestUser@HomeOffice:~$")
 		cmdLineInput := scanner.Text()
 		//Checks the input to the map to see if a function needs to be called
 		for cmds := range cmdList {
 			if cmdLineInput == cmds{
-				//just here for testing purposes, will remove
-				fmt.Println("That's a command my dude")
 				//actual function call happens here using map, not user input
-				fmt.Println(cmdList[cmds])
+				cmdList[cmds](&output)
+				fmt.Print(output)
 			}
 		}
+		fmt.Print("guestUser@HomeOffice:~$")
 	}
 	//error checking for the scanner
 	if scanner.Err() != nil {
