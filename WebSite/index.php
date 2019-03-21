9ba260a45dfefd6343f57c7c76b7195dbc329fb6
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-03-21 16:09:36 -0500

    Dummy Webpage
    Design Pending but is currently template for "fake" website that will exist on the open port

---
+++ WebSite/index.php
@@ -0,0 +1,27 @@
+?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>Welcome to my website!</h2>
+	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+
+	<p>
+		Hey man this is totally working now!
+	</p>
+	<p>
+		Check me out with my flashy webpage fuck yeah!!!
+	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
