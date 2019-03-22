678bb765a5078a2d97c554014c8f8535654a771f
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-03-22 15:38:06 -0500

    Uploaded images page
    Page will populated with photos later

---
+++ WebSite/pictures.php
@@ -0,0 +1,23 @@
+?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>Welcome to my website!</h2>
+	
+	<p>
+		I'm going to add images here I promise. 
+	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
