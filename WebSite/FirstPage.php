825192c9251c73441a3cb74948bf618c69c17a8d
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-03-22 15:23:26 -0500

    Updated Firstpage
    Mirrored page to conform to other webpages present in directory

--- WebSite/FirstPage.php
+++ WebSite/FirstPage.php
@@ -1,3 +1,23 @@
-<?php
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
 
-echo "Hi everyone here's my webpage!";
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>Go Fuck Yourself</h2>
+
+	<p>
+		Do me a huge favor and just get off my webpage you moron....
+	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
