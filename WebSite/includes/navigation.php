050a7318ff0c430ef336090e7c6a1953eb9445b6
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-03-25 16:07:47 -0500

    Migrating folders
    Renamed directory

---
+++ WebSite/includes/navigation.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<ul class="nav nav-pills">
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "FirstPage") {?>active<?php }?>" href="FirstPage.php">First Page</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Pictures") {?>active<?php }?>" href="pictures.php">Pics</a>
+	  </li>	 
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact</a>
+	  </li>
+ 	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Uploads") {?>active<?php }?>" href="upload.php">Whoo Buddy</a>
+	  </li>	</ul>
+</div>
