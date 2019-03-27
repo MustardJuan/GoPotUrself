6ac01a1b9a039e2ef162ed5dbf3b766cca7b1e82
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-03-27 13:49:34 -0500

    Fixed output issue on connection and reformatting
    "Sorry your file wasnt uploaded" no longer appears by default and the code has been restructured for ease of readability and for our purposes

--- WebSite/upload.php
+++ WebSite/upload.php
@@ -11,32 +11,19 @@
 
 <div class="container" id="main-content">
 <?php
-$target_dir = "images/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    /*if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }*/
-    $uploadOk = 1;
-}
-if ($uploadOk == 0) {
-   echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-       echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-   } else {
-       echo "Sorry, there was an error uploading your file.";
-   }
-}
+	$target_dir = "images/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	
+	// Try to upload file
+	if(isset($_POST["submit"])) {
+   		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+       			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+   		} else {
+       			echo "Sorry, there was an error uploading your file.";
+   		}
+	}	
 ?>
 <body>
 
