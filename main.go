1ad3d44683519207e759989f04e0fcf89ecf07e2
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-04-05 17:51:12 -0500

    augmented for new structure
    Removed buffer and loop to read in continuous input from user. Function is now called to lookup reverse shell input and then output the correct text. Println's need to be removed as well I believe.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import "bufio"
 //Declared types for map of function calls made by user, must return string and take no args
 type vanilla func(*string)
 
-func main() {
+func Main(cmdLineInput string) {
 	output := ""
 	//Defined Map data structure of strings using type vanilla
 	//Key, Value similar to dictionary can have more added at a later time
@@ -17,21 +17,16 @@ func main() {
 		"ls":       Ls,
 		"pwd":	    Pwd,
 	}
-	//Scanner class to indefinetly read in input from user
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("guestUser@HomeOffice:~$")
-	for scanner.Scan() {
-		cmdLineInput := scanner.Text()
-		//Checks the input to the map to see if a function needs to be called
-		for cmds := range cmdList {
-			if cmdLineInput == cmds {
-				//actual function call happens here using map, not user input
-				cmdList[cmds](&output)
-				fmt.Print(output)
-			}
+	
+	//Checks the input to the map to see if a function needs to be called
+	for cmds := range cmdList {
+		if cmdLineInput == cmds {
+			//actual function call happens here using map, not user input
+			cmdList[cmds](&output)
+			fmt.Print(output)
 		}
-		fmt.Print("guestUser@HomeOffice:~$")
 	}
+	fmt.Print("guestUser@HomeOffice:~$")
 	//error checking for the scanner
 	if scanner.Err() != nil {
 		fmt.Println("Oops dawg")
