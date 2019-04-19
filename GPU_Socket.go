006f25ee20936135848ec75e527e7367c531a165
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-04-18 21:43:17 -0500

    Updated to make reverse shell display on connect
    Fixed the initial output the reverse shell gives
    Havent tested with entire code just seperately so may need to do a version rollback if any issues come up
    Also cmdHandler will be pushed tomorrow

--- GPU_Socket.go
+++ GPU_Socket.go
@@ -3,6 +3,8 @@ package main
 import "net"
 import "fmt"
 import "bufio"
+import "time"
+import "rand"
 import "strings"
 
 /*
@@ -21,11 +23,28 @@ func main() {
 	//connect to this socket
 	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
 	
-	//read in input from stdin
-	output := "Hi, yes, hello, You have a reverse shell MY dude"
+	/*
+	Displays the initial reverse shell, improvements:
+	1.Formatting isn't perfect, time is awkward
+	2. Fixed the random time generator, maybe few hours behind normal time? 
+	3. Keep displaying dollar sign, should just append to output at bottom of loop
+	*/
+	currentTime := time.Now()
+	month := currentTime.Month()
+	weekday := currentTime.Weekday()
+	dayOut := weekday.String()[0:3]
+	monthOut := month.String()[0:3]
+
+	output := "Linux f6651192effa 4.9.125-linuxkit #1 SMP " + dayOut + " " + monthOut + " " + 
+	weekday.String() + " 08:20:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux\n" +
+	time.Unix(rand.Int63n(24),rand.Int63n(60)).String() + " up 10 min, 0 users, load average: 0.00, 0.02, 0.00\n" + 
+	"USER	TTY	FROM		LOGIN@	IDLE	JCPU	PCPU	WHAT\n" + 
+	"uid=33(www-data) gid=33(www-data) groups=33(www-data)\n" +
+	"/bin/sh: 0: can't access tty; job control turned off\n" + 
+	"$ " 
     
 	// send to socket
-	fmt.Fprintf(conn, output + "\n")
+	fmt.Fprintf(conn, output)
 	
 	for { 
     
