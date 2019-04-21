149e3cd5415bb148eabc4b8e04b1f288364bc8a6
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-04-21 13:09:53 -0500

    updated for new format

--- shell/df.go
+++ shell/df.go
@@ -1,7 +1,7 @@
 package shell
 
-func Df() string {
-	output := ("Filesystem     1K-blocks    Used Available Use% Mounted on\n" +
+func Df(output *string) {
+	output += ("Filesystem     1K-blocks    Used Available Use% Mounted on\n" +
     "overlay         61255492 1886312  56227856   4% /\n" +
     "tmpfs              65536       0     65536   0% /dev\n" +
     "tmpfs            1023372       0   1023372   0% /sys/fs/cgroup\n"+
@@ -9,6 +9,4 @@ func Df() string {
     "shm                65536       0     65536   0% /dev/shm\n" +
     "tmpfs            1023372       0   1023372   0% /proc/acpi\n" +
     "tmpfs            1023372       0   1023372   0% /sys/firmware\n")
-	
-    return output
 }
