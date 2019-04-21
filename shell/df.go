3f2c575c92bc0fdeaa65a4724deda3e979f5d899
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-04-21 14:10:21 -0500

    fixed pointer

--- shell/df.go
+++ shell/df.go
@@ -1,7 +1,7 @@
 package shell
 
 func Df(output *string) {
-	output = ("Filesystem     1K-blocks    Used Available Use% Mounted on\n" +
+	*output = ("Filesystem     1K-blocks    Used Available Use% Mounted on\n" +
     "overlay         61255492 1886312  56227856   4% /\n" +
     "tmpfs              65536       0     65536   0% /dev\n" +
     "tmpfs            1023372       0   1023372   0% /sys/fs/cgroup\n"+
