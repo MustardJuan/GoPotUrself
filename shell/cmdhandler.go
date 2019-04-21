1853135dae3864e2d777789ab3f78fc3fe77edcb
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-04-21 13:04:47 -0500

    updated for deployment
    Changed command structure to accomadate future flags

--- shell/cmdhandler.go
+++ shell/cmdhandler.go
@@ -1,9 +1,10 @@
 package shell
 
-//Declared types for map of function calls made by user, must return string and take no args
-type vanilla func() string
+import "strings"
 
-// CmdLookup searches map for command entered by user and executes
+//Declared types for map of function calls made by user, must return string and take no args
+type vanilla func(*string)
+
 func CmdLookup(cmdLineInput string) string {
 
 	//Defined Map data structure of strings using type vanilla
@@ -12,19 +13,18 @@ func CmdLookup(cmdLineInput string) string {
 		"ifconfig": Ifconfig,
 		"ps":       Ps,
 		"ls":       Ls,
-		"pwd":      Pwd,
-		"df":       Df,
-		"uname":    Uname,
+		"pwd":	    Pwd,
 	}
-
+	
 	//Checks the input to the map to see if a function needs to be called
+	cmdFields := strings.Fields(cmdLineInput)
 	for cmds := range cmdList {
-		if cmdLineInput == cmds {
+		if cmdFields[0] == cmds {
 			//actual function call happens here using map, not user input
-			output := cmdList[cmds]()
-			return output
+			cmdList[cmds](&cmdLineInput)
+			return cmdLineInput
 		}
 	}
 	//if the command isnt present we just tell them they dont have the privs for that command
-	return "Something Something you need to priv esc\n"
+	return "-bash: " + cmdLineInput + ": command not found\n" 
 }
