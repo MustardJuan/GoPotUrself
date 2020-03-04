6030316ee1ae19859c17e3fa4048f06101485416
MustardJuan <45611195+MustardJuan@users.noreply.github.com>
2020-03-03 20:36:08 -0600

    Tried my best to clean this up
    Fixed the formatting on the shell pop
    Reformatted some things
    Added a port argument handler

--- GPU_socket.go
+++ GPU_socket.go
@@ -8,41 +8,56 @@ import (
 	"strings"
 	"math/rand"
 	"time"
+	"strconv"
 	"github.com/GoPotUrself/shell"
 )
 
 func main() {
 	
-	
+	ipAddress := os.Args[1]
+	port := ":8080"
+	if len(os.Args) > 2 {
+		port = ":" + os.Args[2]
+	}
+
 	//connect to this socket
-	address := os.Args[1] + ":8080"
+	address := ipAddress + port
 	conn, _ := net.Dial("tcp", address)
-	
 	
 	currentTime := time.Now()
 	month := currentTime.Month()
 	weekday := currentTime.Weekday()
+	year := strconv.Itoa(currentTime.Year())
 	dayOut := weekday.String()[0:3]
 	monthOut := month.String()[0:3]
+	timeOut := currentTime.String()[11:19]
 
-	output := "Linux f6651192effa 4.9.125-linuxkit #1 SMP " + dayOut + " " + monthOut + " " + 
-	weekday.String() + " 08:20:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux\n" +
-	time.Unix(rand.Int63n(24),rand.Int63n(60)).String() + " up 10 min, 0 users, load average: 0.00, 0.02, 0.00\n" + 
-	"USER	TTY	FROM		LOGIN@	IDLE	JCPU	PCPU	WHAT\n" + 
-	"uid=33(www-data) gid=33(www-data) groups=33(www-data)\n" +
-	"/bin/sh: 0: can't access tty; job control turned off\n" + 
-	"$ " 
-    
+	output := "Linux f6651192effa 4.9.125-linuxkit #1 SMP " + 
+		dayOut + 
+		" " + 
+		monthOut + 
+		" " + 
+		weekday.String() + 
+		" " + 
+		timeOut + 
+		" UTC " + 
+		year + 
+		" x86_64 x86_64 x86_64 GNU/Linux\n" +
+		time.Unix(rand.Int63n(24),rand.Int63n(60)).String() + 
+		" up 15 min, 1 users, load average: 0.00, 0.02, 0.00\n" + 
+		"USER	TTY	FROM		LOGIN@	IDLE	JCPU	PCPU	WHAT\n" + 
+		"uid=33(www-data) gid=33(www-data) groups=33(www-data)\n" +
+		"/bin/sh: 0: can't access tty; job control turned off\n" + 
+		"$ " 
+	
 	// send to socket
 	fmt.Fprintf(conn, output)
-	
 	for { 
     
 		// listen for reply and then display it to the web app owner
 		message, err := bufio.NewReader(conn).ReadString('\n')
-		
-		if err != nil {
 
+		if err != nil {
 			fmt.Println(err)
 			return
 		}
@@ -50,9 +65,7 @@ func main() {
 		output := strings.TrimSpace(string(message))
 		fmt.Print("Message from server: " + message)
 		
-		// send the fake output to the malicious server
-		output = shell.CmdLookup(output) + "$ "
+		//output = shell.CmdLookup(output) + "$ "
 		fmt.Fprintf(conn, output)
-
 	}
 }
