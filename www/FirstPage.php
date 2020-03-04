aa8c552463ca8b63b95241d7a2b29f337b023757
MustardJuan <45611195+MustardJuan@users.noreply.github.com>
2020-03-03 18:29:35 -0600

    Changed some garbage
    Edited this because it had some corny lines in there :l

--- www/FirstPage.php
+++ www/FirstPage.php
@@ -10,13 +10,10 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>All My Deepest Thoughts</h2>
+	<h2>Test Page for Job Stuff</h2>
 
 	<p>
-		Yesterday at work I ran into David and he was being a jerk. So later on when we were working on some other things I just ghosted him. 
-		This blogging thing is super easy I wonder what else I could add to really make this look as flashy as possible. Maybe a backdrop for 
-		the giant empty space of white that I have. Or I could start doing recipes I just have to make the new page for that. Ill just change 
-		contact to recipes and leave my information somewhere else! Maybe on the home page instead? This is so much fun!!!! XD 
+		Hey does this actually work?
 	</p>
 </div>
 
